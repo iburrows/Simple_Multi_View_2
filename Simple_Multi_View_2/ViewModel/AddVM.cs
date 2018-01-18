e8c1ec94968ebb8a3922ea9a5bff8d775969c1a7
Ian <ianburrows.sa@gmail.com>
2018-01-18 14:42:52 +0100

    Fin

---
+++ Simple_Multi_View_2/ViewModel/AddVM.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Multi_View_2.ViewModel
+{
+    public class AddVM: ViewModelBase
+    {
+        public string Message { get; set; }
+        public RelayCommand SendBtn { get; set; }
+
+        Messenger messenger = SimpleIoc.Default.GetInstance<Messenger>();
+        public AddVM()
+        {
+            SendBtn = new RelayCommand(()=> 
+            {
+                messenger.Send<PropertyChangedMessage<string>>(new PropertyChangedMessage<string>(null, Message, ""));
+            });
+        }
+
+    }
+}
---
+++ Simple_Multi_View_2/ViewModel/ListVM.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Multi_View_2.ViewModel
+{
+    public class ListVM:ViewModelBase
+    {
+        public ObservableCollection<string> MessengerList { get; set; }
+
+        Messenger messenger = SimpleIoc.Default.GetInstance<Messenger>();
+        public ListVM()
+        {
+            MessengerList = new ObservableCollection<string>();
+
+            messenger.Register<PropertyChangedMessage<string>>(this, AddToList);
+        }
+
+        private void AddToList(PropertyChangedMessage<string> message)
+        {
+            MessengerList.Add(message.NewValue);
+        }
+    }
+}
